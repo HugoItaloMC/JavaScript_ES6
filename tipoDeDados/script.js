be75151407567606f43c79809205acb67e0a13fc
Hugo Italo Magalhães Correia <magalhaeshugocorreia@gmail.com>
2021-11-15 17:39:02 -0300

    update
    updates : EstudoWeb, VariaveisJS, tipoDeDadosJS

---
+++ tipoDeDados/script.js
@@ -0,0 +1,23 @@
+ /*
+   Exercicios Aula Tipo de Dados
+   */
+
+// Declare uma variável contendo uma string
+var nome = 'Hugo ';
+
+// Declare uma variável contendo um numero dentro de uma string
+var numero = '1001';
+
+// Declare uma variável com a sua idade 
+var idade = 28;
+
+// Declare duas variáveis 
+// Uma com seu nome e outra com sobrenome e some ambas
+var sobreNome = `${nome} Italo`;
+
+// Coloque a seguinte frase em uma variável: 'It`s time'
+var frase = "It's time"
+
+// verifique que tipo da variável contém seu nome
+console.log(typeof nome);
+console.log(sobreNome, '\n',numero, '\n',idade, '\n',frase);
