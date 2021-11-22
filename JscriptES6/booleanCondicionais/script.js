81439d9a3307588578f0ff6d862a7dc515b5aa4a
Hugo Italo Magalhães Correia <magalhaeshugocorreia@gmail.com>
2021-11-22 11:43:50 -0300

    updates
    updates: booleanosCondicionais && functions

--- JscriptES6/booleanCondicionais/script.js
+++ JscriptES6/booleanCondicionais/script.js
@@ -1,19 +1,58 @@
-/* Boolean possui dois valores :
-    false, true
+// Verificar se a sua idade é maior do que a de algum parente
+// Dependendo do resultado no console retorne condicional
 
-    Estrutura Condicionais if(), else, else if() :
+var minhaIdade = 28 ; 
+var idadeParente = 30 ;
 
-
-
-Os valores dentro dos parenteses sempre será avaliado entre false ou true.
-*/
-
-var p = false ;
-
-if(p){
-    console.log('The True') ;
-     x = 10 ;
-} else {
-    console.log('The false')
+if (minhaIdade > idadeParente) {
+    console.log('Maior');
+}else if(minhaIdade === idadeParente) {
+    console.log('igual');
+}else {
+    console.log('menor')
 }
-console.log(x) // retorno da variável 'x' acontece se o bloco condicional for true
+
+// Qual valor é retornado na expressão seguinte :
+
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2) ; 
+console.log(expressao);
+
+// Veririfique se as seguintes variáveis são Truthy ou Falsy
+
+var nome = 'André';
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+
+console.log(!!nome, !!idade,!!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta)
+
+// Compare o total das variáveis seguintes : 
+var brasil = 207 ;
+var china = 1340 ; 
+
+if (brasil > china) {
+    console.log('Brasil tem mais habitantes');
+}else{
+    console.log('Brasil tem menos habitantes');
+}
+
+// O que irá aparecer no console? :
+
+
+if(('Gato' === 'gato')&&(5 > 2)){
+    console.log('True');
+}else{
+    console.log('False');
+}
+
+
+// O que irá aparecer no console ?
+
+
+
+if(('gato' === 'gato')&&(5 > 2)){
+    console.log('Gato' && 'Cão');
+}else{
+    console.log('False');
+}
---
+++ JscriptES6/functions-js/script.js
@@ -0,0 +1,27 @@
+/**
+Funcões 
+    Bloco de código que pode ser executado e reutilizado.
+Valores podem ser passados por uma funcão e a mesma retorna
+outro valor.
+
+function areaQuadrada(lado){
+    return lado * lado;
+}
+
+
+##
+areaQuadrada(4);
+areaQuadrada(6);
+areaQuadrada(2);
+## Chamada de function declaration
+*/
+
+
+function areaQuadrada(lado){
+    return lado * lado;
+}
+
+console.log(areaQuadrada(4));
+areaQuadrada(6);
+areaQuadrada(2);
+
