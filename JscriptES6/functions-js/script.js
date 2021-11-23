826b47124ceac80326cbc7352b95596819fef129
Hugo Italo Magalhães Correia <magalhaeshugocorreia@gmail.com>
2021-11-22 21:59:53 -0300

    update script.js : functions
    functions
    : script.js

--- JscriptES6/functions-js/script.js
+++ JscriptES6/functions-js/script.js
@@ -33,4 +33,76 @@ function imc(peso, altura) {
     console.log(imc);
 } 
 
-imc(80, 1.8)
+imc(80, 1.8);
+
+
+function terceiraIdade(idade) {
+    if ( typeof idade !== 'number') {
+        return 'Por favor preencha com número'
+    }else if (idade >= 60) {
+        return true ;
+    } else {
+        return false ;
+    }
+}
+
+console.log(terceiraIdade('oi'));
+
+/*
+ Variáveis e funcões definidas dentro de um bloco ' {} ', não são visíveis
+fora dele.
+
+function precisoVisitar(paisesVisitados){
+    var totalPaises = 193 ;
+    return `Ainda Faltam ${totalPaises - paisesVisitados} paises`;
+}
+
+console.log(totalPaises) ;  // Erro totalPaises não definido.
+
+*/
+
+
+function precisoVisitar(paisesVisitados){
+    var totalPaises = 193 ;
+    return `Ainda Faltam ${totalPaises - paisesVisitados} paises`;
+}
+
+/**
+Escopo léxico, funões conseguem acessar variáveis que foram criadas no contexto pai.
+
+
+var profissao = 'Design' ;
+
+
+function dados() {
+    var nome = 'André' ;
+    var idade = 18 ;
+    function outroDados(){
+        var endereco = 'Rio de Janeiro' ; 
+        var idade = 29 ;
+        return `${nome}, ${idade}, ${endereco}, ${profissao}` ;
+    }
+    return outroDados();
+}
+
+
+ */
+
+
+
+var profissao = 'Design' ;
+
+
+function dados() {
+    var nome = 'André' ;
+    var idade = 18 ;
+    function outroDados(){
+        var endereco = 'Rio de Janeiro' ; 
+        var idade = 29 ;
+        return `${nome}, ${idade}, ${endereco}, ${profissao}` ;
+    }
+    return outroDados();
+}
+
+console.log(dados()); // retorna os dados
+// outrosDados(); // Retorna erro
